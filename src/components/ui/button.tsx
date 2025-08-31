e00088081931ff203490f3e470fe89f667e82fde
mdsahilhussain <mdsahil.hussain7@gmail.com>
2025-08-31 21:59:08 +0530

    Optimized more button, avater and image component, add sparkline component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,14 +1,25 @@
+import { memo, type ReactNode } from 'react';
+import { cn } from '../../lib/utils';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   type?: 'button' | 'submit' | 'reset';
-  children: string;
+  children: ReactNode;
   onClick?: () => void;
 }
-const Button = ({ type = 'submit', onClick, children, ...rest }: ButtonProps) => {
+const Button: React.FC<ButtonProps> = ({ type = 'submit', onClick, children, ...rest }) => {
   return (
-    <button type={type} onClick={onClick} {...rest}>
+    <button
+      type={type}
+      onClick={onClick}
+      {...rest}
+      className={cn(
+        'px-4 py-2 bg-(--btn-bg) text-(--btn-text) rounded hover:bg-(--btn-bg-hover) transition flex items-center',
+        children
+      )}
+    >
       {children}
     </button>
   );
 };
 
-export default Button;
+const ButtonMemo = memo(Button);
+export default ButtonMemo;
